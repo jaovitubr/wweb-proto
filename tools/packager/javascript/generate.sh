c9ad9f8467e02b1e0786c4d75e7a7b3b3d25ce04
jaovitubr <joaovmachado10@gmail.com>
2024-06-01 17:47:22 -0300

    created out/packages folder

--- tools/packager/javascript/generate.sh
+++ tools/packager/javascript/generate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-JS_OUT=$OUT_DIR/javascript
+OUT=$OUT_DIR/javascript
 
-rm -rf $JS_OUT
-mkdir $JS_OUT
+rm -rf $OUT
+mkdir $OUT
 
-cp package.json $JS_OUT/package.json
-cp readme.md $JS_OUT/readme.md
+cp package.json $OUT/package.json
+cp readme.md $OUT/readme.md
 
-sed -i 's/{{WA_VERSION}}/'"$NEWEST_WA_VERSION"'/g' $JS_OUT/package.json
-sed -i 's/{{WA_VERSION}}/'"$NEWEST_WA_VERSION"'/g' $JS_OUT/readme.md
+sed -i 's/{{WA_VERSION}}/'"$NEWEST_WA_VERSION"'/g' $OUT/package.json
+sed -i 's/{{WA_VERSION}}/'"$NEWEST_WA_VERSION"'/g' $OUT/readme.md
 
-pbjs -t static-module -w commonjs -o $JS_OUT/index.js $OUT_DIR/proto/*.proto
-pbts -o $JS_OUT/index.d.ts $JS_OUT/index.js
+pbjs -t static-module -w commonjs -o $OUT/index.js $OUT_DIR/proto/*.proto
+pbts -o $OUT/index.d.ts $OUT/index.js
