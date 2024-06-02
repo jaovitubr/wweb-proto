e7b6dce11818fd317af96c0483e66eae68bbdbc3
jaovitubr <joaovmachado10@gmail.com>
2024-06-02 12:13:44 -0300

    added support to esmodules and commonjs

--- tools/packager/javascript/generate.sh
+++ tools/packager/javascript/generate.sh
@@ -11,5 +11,9 @@ cp readme.md $OUT/readme.md
 sed -i 's/{{WA_VERSION}}/'"$NEWEST_WA_VERSION"'/g' $OUT/package.json
 sed -i 's/{{WA_VERSION}}/'"$NEWEST_WA_VERSION"'/g' $OUT/readme.md
 
-pbjs -t static-module -w commonjs -o $OUT/index.js $OUT_DIR/proto/*.proto
-pbts -o $OUT/index.d.ts $OUT/index.js
+pbjs -t static-module -w commonjs -o $OUT/index.cjs $OUT_DIR/proto/*.proto
+pbjs -t static-module -w es6 -o $OUT/index.mjs $OUT_DIR/proto/*.proto
+pbts -o $OUT/index.d.ts $OUT/index.cjs
+
+uglifyjs --compress --beautify -o $OUT/index.cjs $OUT/index.cjs
+uglifyjs --compress --beautify -o $OUT/index.mjs $OUT/index.mjs
